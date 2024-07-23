b60257142b4976e755b467eaa6968b2cadbd1701
Akshaymr83 <akshaymr83@gmail.com>
2024-07-23 12:03:47 +0530

    Delete nodemon

--- backend/index.js
+++ backend/index.js
@@ -68,7 +68,8 @@ io.on('connection', (socket) => {
   });
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/FOOD')
+ const url = process.env.MONGODBURL
+mongoose.connect(url)
 .then(()=>{
     console.log('mongoose is connected');
 })
